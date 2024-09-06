8413803707e4540702bd3675eabb9049be0a2767
fisayo-dev <olufisayobadina@gmail.com>
2024-09-06 12:27:18 +0100

    Handled the Page not found functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "../src/pages/Home";
 import AppLayout from "./layouts/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <Router>
       <Routes element={<AppLayout />}>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+const PageNotFound = () => {
+  return (
+    <div className="bg-color text-center h-[100vh] justify-items-center items-center grid py-10 text-slate-300">
+      <div className="grid gap-5">
+        <h2 className="text-6xl">404</h2>
+        <p className="text-sm">Sorry, this page does not exist</p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;

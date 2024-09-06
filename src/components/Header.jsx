699dbb422d4653189a97c12d73969dbc6c774cdb
fisayo-dev <olufisayobadina@gmail.com>
2024-09-07 00:30:16 +0100

    Modified the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "./Button";
 
 const Header = () => {
@@ -5,7 +6,9 @@ const Header = () => {
     <div className="fixed top-0 w-full z-10 bg-transparent backdrop-blur-md text-white px-5 py-3">
       <div className="app-container">
         <div className="flex justify-between items-center">
-          <h2 className="text-2xl font-bold">Intrix</h2>
+          <Link to="/">
+            <h2 className="text-2xl font-bold">Intrix</h2>
+          </Link>
           <div className="flex text-center justify-center gap-3">
             <p className="cursor-pointer developer-drop-down-text relative">
               <p className="">Developer</p>

5778666530351654d3946411014e4155901e4b4f
fisayo-dev <olufisayobadina@gmail.com>
2024-09-06 11:46:12 +0100

    Initialized Feature component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,11 @@
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="container mx-auto px-4">
+      <div className="grid gap-2">
+        <h2 className="text-center font-bold text-3xl">Features</h2>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;

911b51104d1105eda15263c4787b780ceb23fcc3
fisayo-dev <olufisayobadina@gmail.com>
2024-09-06 11:24:37 +0100

    Removed header bottom border

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 w-full z-10 bg-transparent border-b-2 border-slate-600 backdrop-blur-md text-white px-5 py-3">
+    <div className="fixed top-0 w-full z-10 bg-transparent backdrop-blur-md text-white px-5 py-3">
       <div className="container mx-auto px-4">
         <div className="flex  justify-between items-center">
           <h2 className="text-2xl font-bold">QrCode Generator</h2>

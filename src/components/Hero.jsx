b774ae68701b7a392c048afc0107b86b058c1bc7
fisayo-dev <olufisayobadina@gmail.com>
2024-09-06 11:43:36 +0100

    Modified inta a component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import Button from "../components/Button";
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto px-4 ">
+      <div className="grid justify-items-center">
+        <div className="grid gap-4 py-10 text-center">
+          <h2 className="capitalize text-6xl font-bold tracking-tight">
+            The fast QR Code link generator
+          </h2>
+          <p>Just paste link, click generate, wait a little and download.</p>
+          <Button styles="mx-auto">Get started now</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,13 @@
-import Button from "../components/Button";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+
 import AppLayout from "../layouts/AppLayout";
 
 const Home = () => {
   return (
     <AppLayout>
-      <div className="container mx-auto px-4 text-slate-300">
-        <div className="grid justify-items-center">
-          <div className="grid gap-4 py-10 text-center">
-            <h2 className="capitalize text-6xl font-bold tracking-tight">The fast QR Code link generator</h2>
-            <p>Just paste link, click generate, wait a little and download.</p>
-            <Button styles="mx-auto">Get started now</Button>
-          </div>
-        </div>
-      </div>
+      <Hero />
+      <Features />
     </AppLayout>
   );
 };

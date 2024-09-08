1904113d816acf69e0df0661eb790efa5d2c0efd
fisayo-dev <olufisayobadina@gmail.com>
2024-09-08 20:59:22 +0100

    Tweaked Faq and installed qrcode

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -6,36 +6,47 @@ const Faqs = () => {
       <div className="app-container">
         <div className="grid gap-7">
           <h2 className="text-center text-3xl">FAQS</h2>
-          <div className="grid gap-5 justify-items-center">
+          <div className="grid gap-5 mx-auto md:w-3/4 lg:3/5">
             <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Who is the developer of Intrix
               </p>
-              <FaCaretDown />
+              
+              <div className="cursor-pointer">
+                <FaCaretDown />
+              </div>
             </div>
             <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
-              <FaCaretDown />
+              <div className="cursor-pointer">
+                <FaCaretDown />
+              </div>
             </div>
             <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
-              <FaCaretDown />
+              <div className="cursor-pointer">
+                <FaCaretDown />
+              </div>
             </div>
             <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
-              <FaCaretDown />
+              <div className="cursor-pointer">
+                <FaCaretDown />
+              </div>
             </div>
             <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
-              <FaCaretDown />
+              <div className="cursor-pointer">
+                <FaCaretDown />
+              </div>
             </div>
           </div>
         </div>

bea596c39743ffdb02a83b3c4988bfa7cde7a451
fisayo-dev <olufisayobadina@gmail.com>
2024-09-09 00:27:26 +0100

    Working on the scan page

--- src/pages/Scan.jsx
+++ src/pages/Scan.jsx
@@ -2,9 +2,24 @@ import { NavLink } from "react-router-dom";
 import Button from "../components/Button";
 import { FaClipboard } from "react-icons/fa6";
 import { useState } from "react";
+// import QrScanner from 'react-qr-scanner'
 
 const Scan = () => {
-  const [inputField, setInputField] = useState('')
+  const [scanResult, setScanResult] = useState(null);
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  const handleScan = (data) => {
+    if (data) setScanResult(data.text);
+  };
+
+  const handleError = (err) => {
+    console.log(err);
+  };
+
   return (
     <div className="py-10 h-[85vh]">
       <div className="app-container">
@@ -13,19 +28,29 @@ const Scan = () => {
             <h2 className="text-center text-3xl font-bold">
               Place you QR code for Intrix to scan!!
             </h2>
-            <div className="bg-slate-500 p-5 rounded-lg overflow-hidden"></div>
-            <div className="flex items-center bg-slate-200 gap-3 text-slate-700 rounded-lg p-3">
-              <input
-                type="text"
-                placeholder="Paste your link here. e.g https://www.anylink.com"
-                value={inputField}
-                onChange={(e) => setInputField(e.target.value)}
+            {/* <div className="bg-slate-900 rounded-lg overflow-hidden grid justify-items-center">
+              <QrScanner
+                delay={300}
+                style={previewStyle}
+                onError={handleError}
+                onScan={handleScan}
               />
-              <FaClipboard className="cursor-pointer"/>
-            </div>
-            <Button disabled={inputField.trim() == "" ? true : false}>Open Link</Button>
+            </div> */}
+            {scanResult && (
+              <div className="flex items-center bg-slate-200 gap-3 text-slate-700 rounded-lg p-3">
+                <p>{scanResult}</p>
+                <FaClipboard className="cursor-pointer" />
+              </div>
+            )}
+            <Button>
+              Open Link
+            </Button>
             <NavLink
-            to="/generate"  className="text-center py-2 hover:underline cursor-pointer">Generate a QR Code</NavLink>
+              to="/generate"
+              className="text-center py-2 hover:underline cursor-pointer"
+            >
+              Generate a QR Code
+            </NavLink>
           </div>
         </div>
       </div>

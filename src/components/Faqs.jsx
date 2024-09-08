1ca0912999fe90a599dc5a5656adbc7cac869429
fisayo-dev <olufisayobadina@gmail.com>
2024-09-08 20:36:49 +0100

    Cretae the faqs page

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,47 @@
+import { FaArrowDown } from "react-icons/fa6";
+
+const Faqs = () => {
+  return (
+    <div className="py-5">
+      <div className="app-container">
+        <div className="grid gap-7">
+          <h2 className="text-center text-3xl">FAQS</h2>
+          <div className="grid gap-5 justify-items-center">
+            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <FaArrowDown />
+            </div>
+            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <FaArrowDown />
+            </div>
+            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <FaArrowDown />
+            </div>
+            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <FaArrowDown />
+            </div>
+            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <FaArrowDown />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Faqs from "../components/Faqs";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <Faqs />
     </>
   );
 };

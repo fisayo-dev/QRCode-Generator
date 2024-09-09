dad6b84ebd7a033c95e0cdef8cf62752987ab487
fisayo-dev <olufisayobadina@gmail.com>
2024-09-09 22:41:55 +0100

    Modified the developer header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { FaGithub, FaXTwitter, FaDev } from "react-icons/fa6";
+import { SiDailydotdev } from "react-icons/si";
 
 const Header = () => {
   return (
@@ -12,16 +14,47 @@ const Header = () => {
           <div className="flex text-center justify-center gap-3">
             <p className="cursor-pointer developer-drop-down-text relative">
               <p>Developer</p>
-              <div
-                className="hidden developer-drop-down border-2 bg-color border-slate-400  z-90 p-5 rounded-lg"
-              >
+              <div className="hidden developer-drop-down border-2 bg-color border-slate-400  z-90 p-5 rounded-lg">
                 <div className="flex items-center gap-5 w-full">
                   <img src="" alt="" />
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Dolorum optio fugit voluptatum repellat facilis libero
-                    eligendi, dignissimos rerum, aliquam iste consectetur.{" "}
-                  </p>
+                  <div className="grid gap-2">
+                    <p>
+                      Hi, my name is <b>Fisayo Obadina.</b> I am web developer with
+                      over 3 years of experience. My skills are HTML5, CSS3,
+                      JavaScript, React, NodeJS, Git and Tailwind Css.
+                    </p>
+                    <div className="flex items-center gap-2">
+                      <p>Follow me on: </p>
+                      <Link
+                        to="https://app.daily.dev/fisayodev"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <SiDailydotdev />
+                      </Link>
+                      <Link
+                        to="https://www.x.com/fisayocoder"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaXTwitter />
+                      </Link>
+                      <Link
+                        to="https://www.github.com/fisayo-dev"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithub />
+                      </Link>
+                      <Link
+                        to="https://dev.to/fisayocoder"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaDev />
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </p>

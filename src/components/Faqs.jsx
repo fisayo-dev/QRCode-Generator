0dc5f0ac475d9d5a3432940af1be7d6d2d11bcf1
fisayo-dev <olufisayobadina@gmail.com>
2024-09-09 21:20:36 +0100

    Worked on the faq page

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,53 +1,65 @@
+import { useState } from "react";
 import { FaCaretDown } from "react-icons/fa6";
 
 const Faqs = () => {
+  const [openFaqs, setOpenFaqs] = useState([false, false, false, false, false]);
+
+  const toggleFaq = (index) => {
+    const updatedFaqs = openFaqs.map((isOpen, i) =>
+      i === index ? !isOpen : isOpen
+    );
+    setOpenFaqs(updatedFaqs);
+  };
+
+  const faqs = [
+    {
+      question: "Who is the developer of Intrix ?",
+      answer: "Intrix was developed by Fisayo Obadina.",
+    },
+    {
+      question: "What does Intrix do ?",
+      answer: "Intrix helps you create qrcode for any link very fast.",
+    },
+    {
+      question: "Do you need to have an account to get started ?",
+      answer:
+        "No, intrix has no authentication and available to all users everywhere.",
+    },
+    {
+      question: "Can i download a Qr code ?",
+      answer: "Of course. Intrix provided unlimited download per qr code.",
+    },
+    {
+      question: "How many QR codes can I genereate ?",
+      answer: "You can generate as many qr codes as you want",
+    },
+  ];
+
   return (
     <div className="py-5">
       <div className="app-container">
         <div className="grid gap-7">
           <h2 className="text-center text-3xl">FAQS</h2>
           <div className="grid gap-5 mx-auto md:w-3/4 lg:3/5">
-            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
-              <p className="w-full">
-                Who is the developer of Intrix
-              </p>
-              
-              <div className="cursor-pointer">
-                <FaCaretDown />
+            {faqs.map((faq, index) => (
+              <div
+                key={index}
+                className="grid gap-2  p-4 bg-slate-800 rounded-lg "
+              >
+                <div
+                  className={`flex py-2 cursor-pointer ${
+                    openFaqs[index] && " border-b-2 border-slate-500"
+                  } gap-5 items-center`}
+                  onClick={() => toggleFaq(index)}
+                >
+                  <p className="w-full">{faq.question}</p>
+                  <div className="cursor-pointer">
+                    <FaCaretDown />
+                  </div>
+                </div>
+                {openFaqs[index] && <p>{faq.answer}</p>}
               </div>
-            </div>
-            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
-              <p className="w-full">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="cursor-pointer">
-                <FaCaretDown />
-              </div>
-            </div>
-            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
-              <p className="w-full">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="cursor-pointer">
-                <FaCaretDown />
-              </div>
-            </div>
-            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
-              <p className="w-full">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="cursor-pointer">
-                <FaCaretDown />
-              </div>
-            </div>
-            <div className="flex bg-slate-800 gap-5 p-4 rounded-lg items-center justify-betweeen">
-              <p className="w-full">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="cursor-pointer">
-                <FaCaretDown />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>

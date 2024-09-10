e341e85a50b349fc9ecf797f3e6cb7c74ce8b9a9
fisayo-dev <olufisayobadina@gmail.com>
2024-09-10 12:22:04 +0100

    Made the page import to just one file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
-import Home from "../src/pages/Home";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Generate from "./pages/Generate";
-import Scan from "./pages/Scan";
+
+import { PageNotFound, Generate, Scan, Home } from "./pages/index";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-      <Route path="/" element={<AppLayout />}>
-        <Route path="*" element={<PageNotFound />} />
-        <Route index element={<Home />} />
-        <Route path="/generate" element={<Generate /> } />
-        <Route path="/scan" element={ <Scan />} />
-      </Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="*" element={<PageNotFound />} />
+          <Route index element={<Home />} />
+          <Route path="/generate" element={<Generate />} />
+          <Route path="/scan" element={<Scan />} />
+        </Route>
       </>
     )
-  )
-  return  <RouterProvider router={router}/>
+  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import PageNotFound from "./PageNotFound";
+import Generate from "./Generate";
+import Home from "./Home";
+import Scan from "./Scan";
+
+export {
+    PageNotFound,
+    Generate,
+    Scan,
+    Home
+}
